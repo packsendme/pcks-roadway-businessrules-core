8fa69c1f829089d317926ec1cf2c2b9391f612fc
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-11-03 16:47:23 +0100

    ChangeVersion

--- src/main/java/com/packsendme/roadbrewa/roadway/config/Cache_Config.java
+++ src/main/java/com/packsendme/roadbrewa/roadway/config/Cache_Config.java
@@ -6,9 +6,26 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Cache_Config {
 
-	// roadwaymanagerSA-cache
 	@Value(value = "${redis.cache.roadwayBRE_SA}")
 	public String roadwayBRE_SA;
+
+	@Value(value = "${redis.cache.airwayBRE_SA}")
+	public String airwayBRE_SA;
+
+	@Value(value = "${redis.cache.maritimewayBRE_SA}")
+	public String maritimewayBRE_SA;
+
+	@Value(value = "${redis.cache.executeBRE_SA}")
+	public String executeBRE_SA;
+
+	@Value(value = "${redis.cache.tollsfuelBRE_SA}")
+	public String tollsfuelBRE_SA;
+	
+	@Value(value = "${redis.cache.financeCostDeliveryBRE_SA}")
+	public String financeCostDeliveryBRE_SA;
+	
+	@Value(value = "${redis.cache.truckBRE_SA}")
+	public String truckBRE_SA;
 
 
 }
